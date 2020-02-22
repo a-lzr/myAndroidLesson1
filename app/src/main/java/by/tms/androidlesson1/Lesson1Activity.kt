5a3c406341e846e3b4d6f96fa0fdfbca1b5caa83
Alexander <developer.alexander.lazerko@gmail.com>
2020-02-22 16:41:10 +0300

    Lesson1

---
+++ app/src/main/java/by/tms/androidlesson1/Lesson1Activity.kt
@@ -0,0 +1,12 @@
+package by.tms.androidlesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lesson1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson1)
+    }
+}
--- app/src/main/java/by/tms/androidlesson1/MainActivity.kt
+++ app/src/main/java/by/tms/androidlesson1/MainActivity.kt
@@ -1,12 +1,19 @@
 package by.tms.androidlesson1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtnLesson1Click(view: View) {
+        val intent = Intent(this, Lesson1Activity::class.java)
+        startActivity(intent)
     }
 }
